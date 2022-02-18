923ce6678141527f78f2e02ccdf750f9f6d2f0c6
Mahmudul Alam <mahmudula2000@gmail.com>
2022-02-18 10:30:58 +0600

    Added basic Splash Screen

---
+++ lib/screens/splash.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: Image.asset('assets/images/bsb_logo.png'),
+        ),
+      ),
+    );
+  }
+}
